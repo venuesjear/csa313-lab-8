774a405edcdd2c05946c0a6967ae3bc9c1b5b8ca
B222270036 <sugargalsannamjil@outlook.com>
2025-10-21 17:48:09 +0800

    person add meeting

--- src/test/java/edu/sc/csce747/MeetingPlanner/PersonTest.java
+++ src/test/java/edu/sc/csce747/MeetingPlanner/PersonTest.java
@@ -29,4 +29,23 @@ public class PersonTest {
         assertEquals("Sugar", person.getName());
     }
 
+    @Test
+    public void testAddMeeting_validMeeting() throws TimeConflictException {
+        Meeting meeting = new Meeting(1, 1, 10, 12);
+        person.addMeeting(meeting);
+        assertTrue(person.isBusy(1, 1, 10, 12));
+    }
+
+    @Test
+    public void testAddMeeting_conflictingMeeting() throws TimeConflictException {
+        Meeting meeting1 = new Meeting(1, 1, 10, 12);
+        meeting1.setDescription("First Meeting");
+        Meeting meeting2 = new Meeting(1, 1, 11, 13);
+        meeting2.setDescription("Second Meeting");
+        person.addMeeting(meeting1);
+        TimeConflictException exception = assertThrows(TimeConflictException.class,
+                () -> person.addMeeting(meeting2));
+        assertTrue(exception.getMessage().contains("Conflict for attendee Sugar"));
+    }
+
 }
