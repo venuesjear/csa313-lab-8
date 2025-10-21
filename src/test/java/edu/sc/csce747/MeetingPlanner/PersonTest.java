2d796c47aef279787df537e4faae94ce69e40adc
B222270036 <sugargalsannamjil@outlook.com>
2025-10-21 17:17:21 +0800

    person is busy

--- src/test/java/edu/sc/csce747/MeetingPlanner/PersonTest.java
+++ src/test/java/edu/sc/csce747/MeetingPlanner/PersonTest.java
@@ -1,9 +1,28 @@
 package edu.sc.csce747.MeetingPlanner;
 
-//import static org.junit.Assert.*;
-//import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PersonTest {
-	// Add test methods here. 
-    // You are not required to write tests for all classes.
+    private Person person;
+
+    @BeforeEach
+    public void setUp() {
+        person = new Person("Sugar");
+    }
+    @Test
+    public void testIsBusy_freeTime() throws TimeConflictException {
+        assertFalse(person.isBusy(1, 1, 10, 11));
+    }
+
+    @Test
+    public void testIsBusy_occupiedTime() throws TimeConflictException {
+        Meeting meeting = new Meeting(1, 1, 10, 12);
+        person.addMeeting(meeting);
+        assertTrue(person.isBusy(1, 1, 10, 12));
+    }
+
 }
