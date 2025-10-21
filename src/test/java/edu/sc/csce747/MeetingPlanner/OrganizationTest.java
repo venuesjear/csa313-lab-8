a9a9ad76fe43a3c1227d74047f366c1ba76a25f2
B222270036 <sugargalsannamjil@outlook.com>
2025-10-21 17:03:49 +0800

    org get room

--- src/test/java/edu/sc/csce747/MeetingPlanner/OrganizationTest.java
+++ src/test/java/edu/sc/csce747/MeetingPlanner/OrganizationTest.java
@@ -1,11 +1,30 @@
 package edu.sc.csce747.MeetingPlanner;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OrganizationTest {
+    private Organization organization;
+
+    @BeforeEach
+    public void setUp() {
+        organization = new Organization();
+    }
 
     @Test
-    public void testGetRooms() {
-
+    public void testGetRoom_existingRoom() throws Exception {
+        Room room = organization.getRoom("2A01");
+        assertNotNull(room);
+        assertEquals("2A01", room.getID());
     }
+
+    @Test
+    public void testGetRoom_nonExistingRoom() {
+        Exception exception = assertThrows(Exception.class,
+                () -> organization.getRoom("NonExistentRoom"));
+        assertEquals("Requested room does not exist", exception.getMessage());
+    }
+
 }
